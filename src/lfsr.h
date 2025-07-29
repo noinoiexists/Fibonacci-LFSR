0b1bf37dfbf7265ab25e962cae71dacbc1203689
noinoiexists <nithikramesh2006@gmail.com>
2025-07-29 23:10:22 +0530

    added known plaintext attack

--- src/lfsr.h
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-#include <stdlib.h>
-
-#define MAX_LENGTH 32
-
-typedef uint8_t Bit;
-typedef uint32_t LFSRState;
-
-Bit *lfsr_compute(LFSRState state, LFSRState coeff, size_t m, size_t n);
